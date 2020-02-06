1b3ace724a8a841d2749459c2beedcd268d103c6
foobar404 <austinthemichaud@gmail.com>
2020-02-06 11:27:25 -0800

    change model reference path

---
+++ js/snap-ai.js
@@ -0,0 +1,33 @@
+const classifier = ml5.soundClassifier('/snap-model/model.json', modelReady);
+
+function modelReady() {
+    // classify sound
+    classifier.classify(gotResult);
+}
+
+let rebound = false;
+function gotResult(error, result) {
+    if (error) {
+        console.log(error);
+        return;
+    }
+
+    let snap = result.reduce((val, acc) => {
+        return (val.label == "snap") ? val : acc;
+    })
+
+    if (snap.confidence >= .90) {
+        if (!rebound) {
+            toggleLight()
+            rebound = true;
+
+            setTimeout(() => {
+                rebound = false;
+            }, 2000)
+        }
+    }
+}
+
+function toggleLight() {
+    document.querySelector("main").classList.toggle("on");
+}
